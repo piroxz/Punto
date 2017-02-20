38f19c23e29b14eb9fe82195139946b17cd3a6ab
dam1 <dam1@1DAM-01.IES.QUEVEDO-AD>
2017-02-20 14:13:46 +0100

    pnto

--- src/Punto.java
+++ src/Punto.java
@@ -1,4 +1,41 @@
 
 public class Punto {
-
+	
+	private double punto1;
+	private double punto2;
+	
+	
+	public Punto(){
+		punto1 = 0;
+		punto2 = 0;
+	}
+	
+	public Punto(double punto1,double punto2){
+		this.punto1 = punto1;
+		this.punto2 = punto2;
+	}
+	
+	
+	public double getpunto1(){
+		return punto1;
+	}
+	
+	public double getpunto2(){
+		return punto2;
+	}
+	
+	public double calcularDistancia(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
